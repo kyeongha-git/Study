b31938d2fd67d4624f65859ccbe965af4d553cf1
Kyeong Ha Hwang <168719498+kyeongha-git@users.noreply.github.com>
2025-05-13 14:49:38 +0900

    Update token_embeddings.py

--- Transformer/models/embedding/token_embeddings.py
+++ Transformer/models/embedding/token_embeddings.py
@@ -8,8 +8,8 @@ import torch
 import torch.nn
 
 class TokenEmbedding(nn.Embedding):
-    
-    # 시퀀스 -> 정수로 변환하는 클래스.
+    # input: (batch_size, seq_len), output: (batch_size, seq_len, d_model)
+    # 시퀀스 -> 정수로 변환하는 과정은 선행되어야 함. 지금은 정수형으로 변환된 데이터가 들어왔다는 가정.
     # paddnig_idx = 1로 설정하여 같은 배치 내 시퀀스 길이가 다를 때 패딩을 추가하여 정렬
     def __init__(self, vocab_size, d_model):
         super(TokenEmbedding, self).__init__(vocab_size, d_model, padding_idx=1)
